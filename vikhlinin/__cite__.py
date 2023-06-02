553bd24c6392b9348a5aa796b16360c227568b2a
Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>
2023-06-02 10:34:09 +0100

    Create __cite__.py

---
+++ vikhlinin/__cite__.py
@@ -0,0 +1,17 @@
+__cite__ = """@article{Altamura2023rksz,
+       author = {{Altamura}, Edoardo and {Kay}, Scott T. and {Chluba}, Jens and {Towler}, Imogen},
+        title = "{Galaxy cluster rotation revealed in the MACSIS simulations with the kinetic Sunyaev-Zeldovich effect}",
+      journal = {arXiv e-prints},
+     keywords = {Astrophysics - Cosmology and Nongalactic Astrophysics, Astrophysics - Astrophysics of Galaxies, Physics - Computational Physics},
+         year = 2023,
+        month = feb,
+          eid = {arXiv:2302.07936},
+        pages = {arXiv:2302.07936},
+          doi = {10.48550/arXiv.2302.07936},
+archivePrefix = {arXiv},
+       eprint = {2302.07936},
+ primaryClass = {astro-ph.CO},
+       adsurl = {https://ui.adsabs.harvard.edu/abs/2023arXiv230207936A},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+}
+"""
